61a0f5ed03fcd394d66b4572e1ce48083d6fbc6b
Adrian Shephard <Koleg02@mail.ru>
2021-09-21 15:16:48 +0300

    Added new window modes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 #include <iostream>
 
 #include "Vidgets/Button.h"
+void playNewGame();
+void loadGame();
+void briefing();
+void playingGame();
+
+enum class Race {
+    Human,
+    Orc
+};
+
+short Level = -1;
+
+enum class WindowStatus {
+    MainMenu,
+    NewGame,
+    LoadGame,
+    Game,
+    Briefing
+};
 
 //Для подсчёта атрибутов кнопки в отдельные переменные
 size_t buttonPosX = 0, buttonPosY = 0;
@@ -15,80 +34,37 @@ size_t mousePressedX = -1, mousePressedY = -1;
 
 //Музыка и звуки
 sf::Music music;
-sf::Music buttonPressed;
+sf::Sound buttonPressed;
+
+//Настройка окна
+sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
+sf::RenderWindow window(desktopMode, "WARTD", sf::Style::Fullscreen);
+
+//Фон главного меню
+sf::Texture backgroundTexture;
+sf::Sprite backgroundSprite;
+
+//Шрифт
+sf::Font quake;
+
+//Текстуры кнопок
+sf::Texture orcPressed, orcUnpressed, humanPressed, humanUnpressed;
+
+//Состояние окна
+WindowStatus status = WindowStatus::MainMenu;
 
 void MainMenu() {
-
-}
-
-void playNewGame() {
-
-}
-
-void loadGame() {
-
-}
-
-void playingGame() {
-
-}
-
-int main() {
-    //Настройка музыки в главном меню
-    if (!music.openFromFile("Music/Warcraft II - Orc Briefing.flac")) {
-        return -1;
-    }
-    music.setLoop(true);
-    if (!buttonPressed.openFromFile("SFX/ui/buttonPressed.wav")) {
-        return -1;
-    }
-
-    //Настройка окна
-    sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
-    sf::RenderWindow window(desktopMode, "WARTD", sf::Style::Fullscreen);
-
-    //Создание фона главного меню
-    sf::Texture backgroundTexture;
-    if (!backgroundTexture.loadFromFile("Textures/ui/Menu_background_without_title.png")) {
-        return -1;
-    }
-    backgroundTexture.setSmooth(true);
-
-    sf::Sprite backgroundSprite;
-    backgroundSprite.setTexture(backgroundTexture);
-    float factorX = ((float) desktopMode.width) / backgroundTexture.getSize().x,
-          factorY = ((float) desktopMode.height) / backgroundTexture.getSize().y;
-    backgroundSprite.scale(factorX, factorY);
-
-    //Загрузка шрифта
-    sf::Font quake;
-    if (!quake.loadFromFile("Fonts/Quake Cyr.ttf")) {
-        return -1;
-    }
-
-    //Загрузка текстур кнопок
-    sf::Texture orcPressed, orcUnpressed, humanPressed, humanUnpressed;
-    if (!orcPressed.loadFromFile("Textures/ui/Orc/button-large-pressed.png")) {
-        return -1;
-    }
-    if (!orcUnpressed.loadFromFile("Textures/ui/Orc/button-large-normal.png")) {
-        return -1;
-    }
-    if (!humanPressed.loadFromFile("Textures/ui/Human/button-large-pressed.png")) {
-        return -1;
-    }
-    if (!humanUnpressed.loadFromFile("Textures/ui/Human/button-large-normal.png")) {
-        return -1;
-    }
-    orcPressed.setSmooth(true);
-    orcUnpressed.setSmooth(true);
-    humanPressed.setSmooth(true);
-    humanUnpressed.setSmooth(true);
-
-    while (window.isOpen()) {
-
         //Отрисовка объектов на экране
         window.clear(sf::Color::Blue);
+
+        buttonText = "Warcraft Tower Defence";
+        sf::Text text;
+        text.setFont(quake);
+        text.setString(buttonText);
+        buttonPosX = (desktopMode.width - text.getGlobalBounds().width) / 2;
+        buttonPosY = desktopMode.height / 10;
+        text.setPosition(buttonPosX, buttonPosY);
+        window.draw(text);
 
         window.draw(backgroundSprite);
 
@@ -116,17 +92,9 @@ int main() {
         buttonPosY -= 2 * (buttonHeight + 3);
         window.display();
 
-        //Запуск музыки в главном меню
-        if (music.getStatus() == sf::SoundSource::Stopped) {
-            music.play();
-        }
-
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
                 window.close();
             }
             if (event.type == sf::Event::MouseButtonPressed) {
@@ -152,6 +120,96 @@ int main() {
                 mousePressedX = -1;
                 mousePressedY = -1;
             }
+        }
+}
+
+void playNewGame() {
+
+}
+
+void loadGame() {
+
+}
+
+void briefing() {
+
+}
+
+void playingGame() {
+
+}
+
+int main() {
+
+    //Настройка музыки в главном меню
+    if (!music.openFromFile("Music/Warcraft II - Orc Briefing.flac")) {
+        return -1;
+    }
+    music.setLoop(true);
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("SFX/ui/buttonPressed.wav")) {
+        return -1;
+    }
+    buttonPressed.setBuffer(buffer);
+
+    //Создание фона главного меню
+    if (!backgroundTexture.loadFromFile("Textures/ui/Menu_background_without_title.png")) {
+        return -1;
+    }
+    backgroundTexture.setSmooth(true);
+
+    backgroundSprite.setTexture(backgroundTexture);
+    float factorX = ((float) desktopMode.width) / backgroundTexture.getSize().x,
+          factorY = ((float) desktopMode.height) / backgroundTexture.getSize().y;
+    backgroundSprite.scale(factorX, factorY);
+
+    //Загрузка шрифта
+    if (!quake.loadFromFile("Fonts/Quake Cyr.ttf")) {
+        return -1;
+    }
+
+    //Загрузка текстур кнопок
+    if (!orcPressed.loadFromFile("Textures/ui/Orc/button-large-pressed.png")) {
+        return -1;
+    }
+    if (!orcUnpressed.loadFromFile("Textures/ui/Orc/button-large-normal.png")) {
+        return -1;
+    }
+    if (!humanPressed.loadFromFile("Textures/ui/Human/button-large-pressed.png")) {
+        return -1;
+    }
+    if (!humanUnpressed.loadFromFile("Textures/ui/Human/button-large-normal.png")) {
+        return -1;
+    }
+    orcPressed.setSmooth(true);
+    orcUnpressed.setSmooth(true);
+    humanPressed.setSmooth(true);
+    humanUnpressed.setSmooth(true);
+
+    while (window.isOpen()) {
+
+        switch (status) {
+            case WindowStatus::MainMenu:
+                MainMenu();
+                break;
+            case WindowStatus::NewGame:
+                playNewGame();
+                break;
+            case WindowStatus::LoadGame:
+                loadGame();
+                break;
+            case WindowStatus::Briefing:
+                briefing();
+                break;
+            case WindowStatus::Game:
+                playingGame();
+                break;
+        }
+
+
+        //Запуск музыки в главном меню
+        if (music.getStatus() == sf::SoundSource::Stopped) {
+            music.play();
         }
 
 
